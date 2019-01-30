060c5a94736ec3bb2c48c457022764919b4f82d9
Abel <abelderviche@gmail.com>
2019-01-30 03:36:09 -0300

    no drag

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import  GridItem  from './GridItem';
 import Modal from '../Modal';
-import {SortableContainer, SortableElement,arrayMove} from 'react-sortable-hoc';
 
 const getVowels = string => {
     var counter = string.match(/[aeiou]/gi);
@@ -17,19 +16,6 @@ const compare = (a,b) => {
     }
     return b.id - a.id;
 }
-
-
-const SortableItem = SortableElement(({value}) => <GridItem onClick={() => this.openModal(value.title,value.url)} key={value.id} minUrl={value.thumbnailUrl} title={value.title}/>);
-
-const SortableList = SortableContainer(({items}) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} />
-      ))}
-    </ul>
-  );
-});
 
 export default class Grid extends Component {
     constructor() {
@@ -78,13 +64,6 @@ export default class Grid extends Component {
         
     }
 
-    onSortEnd = ({oldIndex, newIndex}) => {
-        this.setState(({imagesList}) => ({
-          imagesList: arrayMove(imagesList, oldIndex, newIndex),
-        }));
-      };
-     
-
   render() {
     let {imagesList,showModal,itemToShow} = this.state;
     return (
@@ -92,8 +71,7 @@ export default class Grid extends Component {
             {showModal?<Modal title={itemToShow.title} url={itemToShow.url} handleCloseModal={this.handleCloseModal}/>:null}
             <div className="row">
             {imagesList && imagesList.length > 0? 
-                //imagesList.map((item)=><GridItem onClick={() => this.openModal(item.title,item.url)} key={item.id} minUrl={item.thumbnailUrl} title={item.title}/>)
-                <SortableList items={imagesList} onSortEnd={this.onSortEnd} />
+                imagesList.map((item)=><GridItem onClick={() => this.openModal(item.title,item.url)} key={item.id} minUrl={item.thumbnailUrl} title={item.title}/>)
             :'Cargando...'}
             </div>
       </div>
